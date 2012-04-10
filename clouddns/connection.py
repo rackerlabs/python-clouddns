a5938f83b7b94137b400267b2efa6425bcef4d9b
Major Hayden <major@mhtx.net>
2012-04-10 00:12:27 -0500

    Additional cleanup and documentation.

--- clouddns/connection.py
+++ clouddns/connection.py
@@ -257,11 +257,20 @@ class Connection(object):
                                       )
         return self.wait_for_async_request(response)
 
-    def import_domain(self, bind_zone_text):
+    def import_domain(self, bind_zone):
+        """
+        Allows for a bind zone file to be imported in one operation.  The
+        bind_zone parameter can be a string or a file object.
+        """
+
+        if type(bind_zone) is file:
+            bind_zone = bind_zone.read()
+
         xml = '<domains xmlns="http://docs.rackspacecloud.com/dns/api/v1.0">'
         xml += '<domain contentType="BIND_9">'
         xml += '<contents>%s</contents>' % bind_zone_text
         xml += '</domain></domains>'
+
         response = self.make_request('POST', ['domains', 'import'], data=xml)
         output = self.wait_for_async_request(response)
 
